bebc06d7cdb6062a2521a56b342287ad3f25b2d6
KimDaegun00 <ab200065@naver.com>
2025-08-09 08:14:55 +0900

    지도api (주변시설) 내부 파일로 수정
    깃허브 html파일을 플러터 내부 에셋 폴더에 있는 kakao_map.html을 호출하도록 바꿈

--- lib/detail_screens/nearby_screen.dart
+++ lib/detail_screens/nearby_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class NearbyScreen extends StatefulWidget {
   const NearbyScreen({Key? key}) : super(key: key);
@@ -14,17 +15,23 @@ class _NearbyScreenState extends State<NearbyScreen> {
   @override
   void initState() {
     super.initState();
-
     _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(
-          'https://kimdaegun00.github.io/example_map/main_map.html'));
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+
+    _loadHtml();
+  }
+
+  Future<void> _loadHtml() async {
+    // assets 폴더의 HTML 파일 로드
+    String fileHtmlContents =
+    await rootBundle.loadString('assets/kakao_map.html');
+    _controller.loadHtmlString(fileHtmlContents);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('카카오맵')),
+      appBar: AppBar(title: const Text("카카오맵")),
       body: WebViewWidget(controller: _controller),
     );
   }

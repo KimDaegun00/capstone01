91574010a22a6e04a3e5acb131aebce770231e16
KimDaegun00 <ab200065@naver.com>
2025-09-30 16:28:37 +0900

    네비, 카카오맵 파일 수정

--- lib/detail_screens/nearby_screen.dart
+++ lib/detail_screens/nearby_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:url_launcher/url_launcher_string.dart';
 
 class NearbyScreen extends StatefulWidget {
   const NearbyScreen({super.key});
@@ -19,9 +21,19 @@ class _NearbyScreenState extends State<NearbyScreen> {
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..addJavaScriptChannel(
         'BackChannel',
-        onMessageReceived: (JavaScriptMessage msg) {
-          if (msg.message == 'goBack') {
-            Navigator.of(context).pop();
+        onMessageReceived: (JavaScriptMessage msg) async {
+          try {
+            final data = jsonDecode(msg.message);
+            if (data['type'] == 'openExternal') {
+              final url = data['url'];
+              if (await canLaunchUrlString(url)) {
+                await launchUrlString(url, mode: LaunchMode.externalApplication);
+              }
+            } else if (data['type'] == 'goBack') {
+              Navigator.of(context).pop();
+            }
+          } catch (e) {
+            print('Invalid message from JS: ${msg.message}');
           }
         },
       );
@@ -29,7 +41,8 @@ class _NearbyScreenState extends State<NearbyScreen> {
   }
 
   Future<void> _loadHtml() async {
-    String fileHtmlContents = await rootBundle.loadString('assets/kakao_map.html');
+    String fileHtmlContents =
+    await rootBundle.loadString('assets/kakao_map.html');
     _controller.loadHtmlString(fileHtmlContents);
   }
 
